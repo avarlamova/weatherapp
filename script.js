270d42063626cc845550f5aead3cecce19a59bca
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-05-12 22:04:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ new Vue ({
       query: '',
       weather: {},
       locationFound: null,
-      skycons: new Skycons({"color":"orange"}),
       city: '',
       state: ''
         }
     }
 })
+
+//const defaultQuery = 'https://api.weatherbit.io/v2.0/current?city=Kiev,UA&key=${2cac64a96a8e9169f0a06a8a52197d1d};
+console.log(fetch('https://api.weatherbit.io/v2.0/current?city=Kiev,UA&key=${2cac64a96a8e9169f0a06a8a52197d1d}'));
