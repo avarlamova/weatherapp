89af1fcedb426a60402e79bac9d7cb07bd3dc7eb
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-05-14 22:47:10 +0300

    API added

--- script.js
+++ script.js
@@ -14,4 +14,9 @@ new Vue ({
 })
 
 //const defaultQuery = 'https://api.weatherbit.io/v2.0/current?city=Kiev,UA&key=${2cac64a96a8e9169f0a06a8a52197d1d};
-console.log(fetch('https://api.weatherbit.io/v2.0/current?city=Kiev,UA&key=${2cac64a96a8e9169f0a06a8a52197d1d}'));
+async function getWeather() {
+    const url = await
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Kiev,UA&appid=2cac64a96a8e9169f0a06a8a52197d1d');
+    const data = await url.json();
+    console.log(data);
+}
