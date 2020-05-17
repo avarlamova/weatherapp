5067323c8f210e792047b9f631f040fd2f918b47
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-05-17 19:05:24 +0300

    API problems fixed

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ new Vue ({
         return {
             api_key: '2cac64a96a8e9169f0a06a8a52197d1d',
       url_base: 'https://api.openweathermap.org/data/2.5/',
-      query: 'Kiev,UA',
+      query: 'Moscow, RU',
       weather: {},
       locationFound: null,
       city: '',
@@ -15,12 +15,15 @@ new Vue ({
         getWeather () {
             fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
               .then(x => {
-                console.log (x.json());
-     //         }).then(this.setResults);
+                  return x.json()
+               // console.log (x.json());
+               }).then(this.setResults);
+          },
+          setResults (results) {
+              this.weather = results;
           }
-        
-)
+
     },
     
-},
 })
+
