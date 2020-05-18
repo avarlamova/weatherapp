034583c14a55e5778e77c7fbd20abbaba527981e
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-05-18 17:34:24 +0300

    initial CSS file

--- script.js
+++ script.js
@@ -4,22 +4,20 @@ new Vue ({
         return {
             api_key: '2cac64a96a8e9169f0a06a8a52197d1d',
       url_base: 'https://api.openweathermap.org/data/2.5/',
-      query: 'Moscow, RU',
+      query: '',
       weather: {},
-      locationFound: null,
-      city: '',
-      state: ''
         }
     },
     methods: {
         getWeather () {
+    
             fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
               .then(x => {
                   return x.json()
-               // console.log (x.json());
-               }).then(this.setResults);
+               //console.log (x.json());
+               }).then(this.setWeather);
           },
-          setResults (results) {
+          setWeather (results) {
               this.weather = results;
           }
 
