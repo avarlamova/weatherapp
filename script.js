31912004f2995cadbc99a3e7f0919f837cc7aa47
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-07-22 23:39:32 +0300

    added styling for different weather types

--- script.js
+++ script.js
@@ -2,26 +2,47 @@ new Vue ({
     el: '#app',
     data() {
         return {
-            api_key: '2cac64a96a8e9169f0a06a8a52197d1d',
-      url_base: 'https://api.openweathermap.org/data/2.5/',
-      query: '',
-      weather: {},
-        }
+        api_key: '2cac64a96a8e9169f0a06a8a52197d1d',
+        url_base: 'https://api.openweathermap.org/data/2.5/',
+        query: '',
+        weather: {},
+            }
+        },
+
+    computed: {
+            isCloudy: function () {
+                if (typeof this.weather.main !='undefined' && this.weather.weather[0].main === 'Clouds')
+                return true;
+            },
+
+            isRainy: function () {
+                if (typeof this.weather.main !='undefined' && this.weather.weather[0].main === 'Rain')
+                return true;
+                },
+
+            isClear: function () {
+                if (typeof this.weather.main !='undefined' && this.weather.weather[0].main === 'Clear')
+                return true;
+                },
+
+            isSnowy: function () {
+                if (typeof this.weather.main !='undefined' && this.weather.weather[0].main === 'Snow')
+                return true;
+                }    
     },
+    
     methods: {
         getWeather () {
     
             fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
               .then(x => {
-                  return x.json()
-               //console.log (x.json());
+                  return x.json();
                }).then(this.setWeather);
           },
-          setWeather (results) {
-              this.weather = results;
-          }
 
+          setWeather (results) {
+            this.weather = results;
+          }
     },
-    
 })
 
