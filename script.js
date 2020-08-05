2aa638b80504284a30dbcadb7f710171b85fd26e
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-08-05 22:23:21 +0300

    updated date and time
    now the app shows date and time at the location you are searching for

--- script.js
+++ script.js
@@ -6,29 +6,15 @@ new Vue ({
         url_base: 'https://api.openweathermap.org/data/2.5/',
         query: '',
         weather: {},
-            }
-        },
-
-    computed: {
-
-            time: function () {
-                let d = new Date();
-                let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-                let day = days[d.getDay()];
-                let date = d.getDate();
-                let month = months[d.getMonth()];
-                return `${day} ${date} ${month}`;
-            },
-
-            date: function () {
-                let t = new Date ();
-                let hours = t.getHours();
-                let minutes = t.getMinutes();
-                return `${hours}:${minutes}`
-            },
+        time_api_key: 'VU47ZXF8EZ0D',
+        time_url: 'http://api.timezonedb.com/v2.1/get-time-zone',
+        latitude: '',
+        longitude: '',
+        time: '',
+        date: '',
+        }
     },
-    
+
     methods: {
         getWeather () {
     
@@ -38,8 +24,13 @@ new Vue ({
                }).then(this.setWeather);
           },
 
-          setWeather (results) {
+        setWeather (results) {
             this.weather = results;
+
+            //setting coordinates for defining time zone
+            this.latitude = this.weather.coord.lat;
+            this.longitude = this.weather.coord.lon;
+            this.getTimeZone();
 
             //different styles for different weather types
 
@@ -54,7 +45,28 @@ new Vue ({
 
             if (typeof this.weather.main !='undefined' && this.weather.weather[0].main === 'Snow')
                 document.querySelector('body').classList.toggle('snowy');
-          }
+          },
+
+        getTimeZone () {
+            fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=${this.time_api_key}&format=json&by=position&lat=${this.latitude}&lng=${this.longitude}`)
+            .then(x => {
+                return x.json();
+             }).then(this.setTimeZone);
+        },
+
+        setTimeZone(fetchedTime) {
+
+            let string = JSON.stringify(fetchedTime);
+            string = string.substring(string.length-21, string.length-5).split(' ');
+            
+            this.time = string[1];
+            let date = string[0].split('-');
+
+            let year = date[0];
+            let month = date[1];
+            let day = date[2];
+            this.date = day+'-'+month + '-' + year;
+        }
     },
 })
 
